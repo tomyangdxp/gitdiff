1c78d347f2a9e39d7b1d783d39a1eaac368f847d
yangtom <yangtom@host.docker.internal>
2023-11-02 11:55:22 -0500

    Made some fixes

---
+++ war-1/src/main/java/Test.java
@@ -0,0 +1,8 @@
+/* HelloWorld.java
+ */
+public class Test
+{
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+	}
+}
---
+++ war-2/src/main/java/Test2.java
@@ -0,0 +1,8 @@
+/* HelloWorld.java
+ */
+public class Test2
+{
+	public static void main(String[] args) {
+		System.out.println("Hello World 2!");
+	}
+}
